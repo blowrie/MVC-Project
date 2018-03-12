e1aebb6d5ef9ae4571491c8f3ed64bdaa043871c
blowrie <blowrie8@gmail.com>
2018-03-12 00:25:22 -0400

    first commit

---
+++ CerealReview/Models/Brand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CerealReview.Models
+{
+    public class Brand
+    {
+        [Key]
+        public int BrandID { get; set; }
+        [Display(Name = "Brand Name")]
+        public string BrandName { get; set; }
+
+        //Navigation Property
+        public virtual ICollection<Brand> Brands { get; set; }
+    }
+}
---
+++ CerealReview/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CerealReview.Models
+{
+    public class Review
+    {
+        [Key]
+        public int CerealID { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Date Reviewed")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime DateReviewed { get; set; }
+        public int Score { get; set; }
+        [Display(Name = "Eat Again? ")]
+        public bool Again { get; set; }
+        public string Description { get; set; }
+
+        [ForeignKey("Brand"), Display(Name = "Brand")]
+        public int BrandID { get; set; }
+        public virtual Brand Brand { get; set; }
+    }
+}
